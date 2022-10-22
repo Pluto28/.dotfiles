793f3d73b65cea05ad6109bc306c033dbadbb8f5
Pluto28 <mendax103@gmail.com>
2022-10-22 07:20:43 -0300

    themes

--- nvim/.config/nvim/lua/statusline.lua
+++ nvim/.config/nvim/lua/statusline.lua
@@ -4,7 +4,7 @@ lualine.setup(
 {
      options = {
           icons_enabled = true,
-          theme = 'solarized_light',
+          theme = 'solarized_dark',
           component_separators = { left = '', right = ''},
           section_separators = { left = '', right = ''},
           disabled_filetypes = {
