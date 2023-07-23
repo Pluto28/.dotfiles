3186762344b0d2a88308ad15081bf781b2b1ed7f
Pluto28 <mendax103@gmail.com>
2023-07-23 13:33:01 -0300

    changes to the config files. Remove the desktop file since we will use the same config for every computer

---
+++ common/.config/nvim/init.lua
@@ -0,0 +1,6 @@
+require('plugins')
+require('settings')   -- ./lua/settings.lua // Functions and options for the editor
+require('cmpcfg')     -- ./lua/cmpconfig.lua
+require('langs.lsp')  -- ./lua/lspconfig.lua ; language server provider conf
+require('statusline') -- statusline configuration using lualine
+require('treeconf')   -- ./lua/treeconf.lua ; Treesitter configuration
